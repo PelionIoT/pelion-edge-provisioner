d6e83a6aff516c81602e22b81bea83654190ab1d
Yash Goyal <yash.goyal@arm.com>
2021-03-23 00:40:55 -0500

    Updated the copyright year

--- example-scripts/meta-pelion-edge/install-new-identity-with-parsec.sh
+++ example-scripts/meta-pelion-edge/install-new-identity-with-parsec.sh
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, Arm Limited and affiliates.
+# Copyright (c) 2021, Arm Limited and affiliates.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
