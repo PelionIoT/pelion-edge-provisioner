c1117c912be8725050b34b790ec73a34113d315d
Yash Goyal <Yash.Goyal@arm.com>
2021-03-22 23:20:05 -0700

    Added new example install scripts which works with Parsec enabled Pelion Edge stack (#38)
    * Added new example install scripts which works with Parsec enabled Pelion Edge stack
    
    * Updated the copyright year

---
+++ example-scripts/meta-pelion-edge/install-new-identity-with-parsec.sh
@@ -0,0 +1,33 @@
+# Copyright (c) 2021, Arm Limited and affiliates.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+set -e
+
+EDGE_GW_CONFIG_DIR="/userdata/edge_gw_config/"
+IDENTITY_JSON_FILE="identity.json"
+PDM_CRED_DIR="/userdata/mbed/mcc_config"
+PDM_CRED_SUBDIR="/userdata/mbed/mcc_config/pst"
+
+# Move the identity.json to location as per maestro config
+mkdir -p $EDGE_GW_CONFIG_DIR
+mv $IDENTITY_JSON_FILE $EDGE_GW_CONFIG_DIR
+echo "Successfully installed identity.json at $EDGE_GW_CONFIG_DIR$IDENTITY_JSON_FILE"
+
+[ -d "$PDM_CRED_SUBDIR" ] && echo "Found device management credentials in PSA trusted storage at $PDM_CRED_DIR." \
+ || echo "Error: Failed to locate device management credentials at $PDM_CRED_SUBDIR does not exists."
+
+echo "Done"
